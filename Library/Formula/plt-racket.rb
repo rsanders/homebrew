243382bc52987f6c65d2af85b437a9332eb8cb01
Robert Sanders <robert@zeevex.com>
2011-07-30 23:06:50 -0400

    attempt to get plt-racket to compile - fails complaining FFI can't load cairo

--- Library/Formula/plt-racket.rb
+++ Library/Formula/plt-racket.rb
@@ -9,10 +9,17 @@ class PltRacket < Formula
   # Don't strip symbols; need them for dynamic linking.
   skip_clean 'bin'
 
+  # depends_on 'cairo'
+
+  fails_with_llvm "Fails with llvm"
+
   def install
+    ENV.gcc_4_2
     Dir.chdir 'src' do
       args = ["--disable-debug", "--disable-dependency-tracking",
-              "--enable-xonx",
+              "--disable-places", "--disable-futures", # temporary, to force it to build on Mac OS X Lion DP3
+              # "--enable-xonx",
+              "--disable-gracket"
               "--enable-shared",
               "--prefix=#{prefix}" ]
 
