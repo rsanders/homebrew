cb96e468a4d5d0519d4b45ff9a8b3f482ae4a579
Robert Sanders <robert@zeevex.com>
2011-07-30 20:20:12 -0400

    build stklos 1.01 w/GCC 4.2

--- Library/Formula/stklos.rb
+++ Library/Formula/stklos.rb
@@ -1,14 +1,17 @@
 require 'formula'
 
 class Stklos < Formula
-  url 'http://www.stklos.net/download/stklos-1.00.tar.gz'
+  url 'http://www.stklos.net/download/stklos-1.01.tar.gz'
   homepage 'http://www.stklos.net/'
-  md5 '3fd4809205871b65882be9b9f7e17090'
+  md5 '2c370627c3abd07c30949b2ee7d3d987'
 
   depends_on 'gmp'
   depends_on 'pcre'
 
+  fails_with_llvm "Fails with LLVM"
+
   def install
+    ENV.gcc_4_2
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
